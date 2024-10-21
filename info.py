08ef52bfae16cf07fee1bb134828cac74cf47532
Utsav Raj <98069663+maharanautsav@users.noreply.github.com>
2024-10-21 20:39:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg h
 PRIME_LOGO = (environ.get('PRIME_LOGO', 'https://iili.io/2FBp5G4.md.jpg'))
 
 # Admins, Channels & Users *
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('1928805318', '6192448438', '6045160821').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('1928805318 6192448438 6045160821').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -40,8 +40,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information *
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "MAHARANA UTSAV")
+DATABASE_URI = environ.get('mongodb+srv://otsearaz:<6N6s66ZqE8oNHjsD>@cluster0.x6dfx.mongodb.net/?retryWrites=true&w=majority', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MAHARANAUTSAV")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # LOG CHANNELS *
