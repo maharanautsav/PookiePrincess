c4721e10dc42e0e4ecc214deda445cc6d607cdbd
Utsav Raj <98069663+maharanautsav@users.noreply.github.com>
2024-10-27 11:26:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg h
 PRIME_LOGO = (environ.get('PRIME_LOGO', 'https://iili.io/2FBp5G4.md.jpg'))
 
 # Admins, Channels & Users *
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('1928805318 6192448438 6045160821').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('1928805318 6192448438 5542295481 6045160821').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
