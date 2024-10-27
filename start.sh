b880f981deb19d1c7c6070882b447bbeff4a332b
Utsav Raj <98069663+maharanautsav@users.noreply.github.com>
2024-10-27 12:50:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/lazyindu/test1.git /LazyPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone https://github.com/maharanautsav/PookiePrincess
 fi
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
